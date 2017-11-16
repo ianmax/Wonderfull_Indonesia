d4f04994dfb4a0e1fb86e0206240a9ae6f941236
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-11-16 17:54:31 +0700

    Update file App.js dan tambah file routing

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app=require("express")();
 const api=require("./routes/app.js");
 app.use("/api",api);
 
+const landing=require("./routes/index");
+app.use("/",landing);
+
+const result=require("./routes/result");
+app.use("/result",result);
+
 app.listen(3000,()=>{
     console.log("Listenning on port 3000");
 })
