7931e174e2a581fc3e8cf67df4ede3d3296085cf
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-11-17 17:33:36 +0700

    Remove console log

--- views/assets/js/result.js
+++ views/assets/js/result.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
                 dataType:"json",
                 data:loginToken,
                 success:function(fromServer){
-                    console.log(fromServer);
                     if(!fromServer.status){ // Jika error terjadi pada backend
                         alert("Something went wrong!");
                         localStorage.removeItem("login_token");
